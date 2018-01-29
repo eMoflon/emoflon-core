2ed272e36ac65acf76794fd74e84bc5c9282558c
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-01-29 16:11:51 +0100

    Bugfix in creating defaults for moflon.properties.xmi

--- org.moflon.core.propertycontainer/src/org/moflon/core/propertycontainer/MoflonPropertiesContainerHelper.java
+++ org.moflon.core.propertycontainer/src/org/moflon/core/propertycontainer/MoflonPropertiesContainerHelper.java
@@ -14,6 +14,7 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -70,7 +71,9 @@ public class MoflonPropertiesContainerHelper
       {
          moflonPropertiesContainer = PropertycontainerFactory.eINSTANCE.createMoflonPropertiesContainer();
          moflonPropertiesContainer.setProjectName(project.getName());
+         moflonPropertiesContainer.setReplaceGenModel(PropertycontainerFactory.eINSTANCE.createReplaceGenModel());
          moflonPropertiesContainer.getReplaceGenModel().setBool(true);
+         save(moflonPropertiesContainer, new NullProgressMonitor());
       }
       return moflonPropertiesContainer;
    }
