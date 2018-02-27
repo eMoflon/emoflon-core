93c3352deb2890c330880c718824430679c5e842
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-02-27 09:13:15 +0100

    Trying to run Eclipse with headless flag

--- shippable.bash
+++ shippable.bash
@@ -5,9 +5,9 @@
 # Date: 2018-02-27
 
 # Emulate X server via X Window Virtual Framebuffer
-apt-get update && apt-get install -y --no-install-recommends xvfb
-Xvfb :1 -screen 0 1360x1024x24 &
-export DISPLAY=:1
+#apt-get update && apt-get install -y --no-install-recommends xvfb
+#Xvfb :1 -screen 0 1360x1024x24 &
+#export DISPLAY=:1
 
 # Prepare environment
 workspacePath=.
@@ -22,11 +22,11 @@ fi
 mvn generate-sources -pl org.moflon.emf.injection,org.moflon.core.releng.target
 
 # Run eMoflon codegen
-$ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath || exit -1
+$ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath -vmargs -Djava.awt.headless=true || exit -1
 
-$ECLIPSE_HOME/eclipse -nosplash -application com.seeq.eclipse.importprojects.headlessimport -data $workspacePath -import $repositoryRoot || exit -1
+$ECLIPSE_HOME/eclipse -nosplash -application com.seeq.eclipse.importprojects.headlessimport -data $workspacePath -import $repositoryRoot -Djava.awt.headless=true || exit -1
 
-$ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath || exit -1
+$ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath -Djava.awt.headless=true || exit -1
 
 # Run Tycho
 mvn clean compile || exit -1
