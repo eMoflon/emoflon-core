6f06cc23d3572b918815f4f4e8f28541f14034ad
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-02-27 07:26:02 +0100

    exit early if something breaks

--- shippable.bash
+++ shippable.bash
@@ -14,13 +14,16 @@ then
 fi
 
 # Run eMoflon codegen
-$ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath
-$ECLIPSE_HOME/eclipse -nosplash -application com.seeq.eclipse.importprojects.headlessimport -data $workspacePath -import $repositoryRoot
-$ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath
+$ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath || exit -1
+
+$ECLIPSE_HOME/eclipse -nosplash -application com.seeq.eclipse.importprojects.headlessimport -data $workspacePath -import $repositoryRoot || exit -1
+
+$ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath || exit -1
 
 # Run Tycho
-mvn clean compile
-mvn integration-test
+mvn clean compile || exit -1
+
+mvn integration-test || exit -1
 
 # Publish JUnit test results
 find . -path "*/target/*/TEST*.xml" -exec cp {} /root/src/github.com/eMoflon/emoflon-core/shippable/testresults/ \;
