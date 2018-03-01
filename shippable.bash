bdaa43a9bd8e46a483d6ba98c3ab0d36a631182f
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-03-01 09:09:04 +0100

    Import all projects from repository

--- shippable.bash
+++ shippable.bash
@@ -21,26 +21,8 @@ fi
 # First trigger the MWE2 workflow using Tycho (not necessary, left here for documentation purposes)
 #mvn generate-sources -pl org.moflon.emf.injection,org.moflon.core.releng.target
 
-#echo "Create empty workspace"
-#$ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath
-
-echo "Import only those projects that require eMoflon EMF codegen."
-importSpecification=""
-for project in org.moflon.core.utilities org.moflon.core.propertycontainer;
-do
-  projectFolder="$repositoryRoot/$project"
-  if [ ! -f "$projectFolder/.project" ];
-  then
-    echo "Expected to find project folder '$projectFolder'"
-    exit -1
-  fi
-  $ECLIPSE_HOME/eclipse -nosplash -application com.seeq.eclipse.importprojects.headlessimport -data $workspacePath -import $projectFolder || exit -1
-  #importSpecification="$importSpecification -import $projectFolder"
-done
-
-#echo "  Import specification: '$importSpecification'"
-#$ECLIPSE_HOME/eclipse -nosplash -application com.seeq.eclipse.importprojects.headlessimport -data $workspacePath $importSpecification || exit -1
-cat $workspacePath/.metadata/.log
+echo "Import all projects."
+$ECLIPSE_HOME/eclipse -nosplash -application com.seeq.eclipse.importprojects.headlessimport -data $workspacePath -import $repositoryRoot || exit -1
 
 echo "Run eMoflon codegen"
 $ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath || exit -1
