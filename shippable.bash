5d7eda3e46c793a9ba10607bf3904c859dd8554a
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-03-01 13:19:38 +0100

    Disable Xvfb command

--- shippable.bash
+++ shippable.bash
@@ -6,8 +6,8 @@
 
 # Emulate X server via X Window Virtual Framebuffer
 apt-get update && apt-get install --yes --no-install-recommends xvfb
-Xvfb :99 &
-export DISPLAY=:99.0
+#Xvfb :99 &
+#export DISPLAY=:99.0
 
 # Prepare environment
 workspacePath=$(pwd)/../eclipseWorkspace
