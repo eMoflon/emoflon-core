06462ba55a3718d25abf53c16fbd4b579bb428bd
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-03-02 14:38:28 +0100

    Add generated code and disable command-line invocation of Eclipse in CI

---
+++ org.moflon.core.propertycontainer/gen/org/moflon/core/propertycontainer/PropertiesValue.java
@@ -0,0 +1,57 @@
+/**
+ */
+package org.moflon.core.propertycontainer;
+
+import org.eclipse.emf.ecore.EObject;
+// <-- [user defined imports]
+// [user defined imports] -->
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object
+ * '<em><b>Properties Value</b></em>'. <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ * <li>{@link org.moflon.core.propertycontainer.PropertiesValue#getValue
+ * <em>Value</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.moflon.core.propertycontainer.PropertycontainerPackage#getPropertiesValue()
+ * @model
+ * @generated
+ */
+public interface PropertiesValue extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Value</b></em>' attribute. <!--
+	 * begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Value</em>' attribute isn't clear, there really
+	 * should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the value of the '<em>Value</em>' attribute.
+	 * @see #setValue(String)
+	 * @see org.moflon.core.propertycontainer.PropertycontainerPackage#getPropertiesValue_Value()
+	 * @model required="true" ordered="false"
+	 * @generated
+	 */
+	String getValue();
+
+	/**
+	 * Sets the value of the
+	 * '{@link org.moflon.core.propertycontainer.PropertiesValue#getValue
+	 * <em>Value</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Value</em>' attribute.
+	 * @see #getValue()
+	 * @generated
+	 */
+	void setValue(String value);
+	// <-- [user code injected with eMoflon]
+
+	// [user code injected with eMoflon] -->
+} // PropertiesValue
--- shippable.bash
+++ shippable.bash
@@ -3,28 +3,32 @@
 # This script contains the CI build workflow for this repository
 # Author: Roland Kluge
 # Date: 2018-02-27
+#
+# NOTE: Most of the CI script was devoted to invoking the eMoflon code generation on the repository.
+#       However, this appears to work only in environments with a UI, currently, which we cannot properly emulate with Xvfb
+#
 
 # Emulate X server via X Window Virtual Framebuffer
-apt-get update && apt-get install --yes --no-install-recommends xvfb
-export DISPLAY=:99.0
+#apt-get update && apt-get install --yes --no-install-recommends xvfb
+#export DISPLAY=:99.0
 
 # Prepare environment
-workspacePath=$(pwd)/../eclipseWorkspace
-repositoryRoot=$(pwd)
-if [ ! -f $repositoryRoot/shippable.yml ];
-then
-  echo "Expect to find emoflore-core repository at $repositoryRoot"
-  exit -1
-fi
+#workspacePath=$(pwd)/../eclipseWorkspace
+#repositoryRoot=$(pwd)
+#if [ ! -f $repositoryRoot/shippable.yml ];
+#then
+#  echo "Expect to find emoflore-core repository at $repositoryRoot"
+#  exit -1
+#fi
 
 # First trigger the MWE2 workflow using Tycho (not necessary, left here for documentation purposes)
-mvn generate-sources -pl org.moflon.emf.injection,org.moflon.core.releng.target
+#mvn generate-sources -pl org.moflon.emf.injection,org.moflon.core.releng.target
 
-echo "Import all projects."
-xvfb-run --server-args="-ac" $ECLIPSE_HOME/eclipse -nosplash -application com.seeq.eclipse.importprojects.headlessimport -data $workspacePath -import $repositoryRoot || exit -1
+#echo "Import all projects."
+#xvfb-run --server-args="-ac" $ECLIPSE_HOME/eclipse -nosplash -application com.seeq.eclipse.importprojects.headlessimport -data $workspacePath -import $repositoryRoot || exit -1
 
-echo "Run eMoflon codegen"
-xvfb-run --server-args="-ac" $ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath || exit -1
+#echo "Run eMoflon codegen"
+#xvfb-run --server-args="-ac" $ECLIPSE_HOME/eclipse -nosplash -application org.eclipse.jdt.apt.core.aptBuild -data $workspacePath || exit -1
 
 echo "Run Tycho (clean compile)"
 mvn clean compile || exit -1
