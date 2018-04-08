f38841fe0355eecc040e722298ca5f94a5cd3ffb
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-04-08 20:30:45 +0200

    Reformatted file with default settings

--- org.moflon.emf.codegen/src/org/moflon/emf/codegen/MoflonGenModelBuilder.java
+++ org.moflon.emf.codegen/src/org/moflon/emf/codegen/MoflonGenModelBuilder.java
@@ -28,174 +28,160 @@ import org.moflon.emf.codegen.dependency.Dependency;
 import org.moflon.emf.codegen.dependency.DependencyTypes;
 import org.moflon.emf.codegen.dependency.SimpleDependency;
 
-public class MoflonGenModelBuilder extends GenModelBuilder
-{
-   protected static final Logger logger = Logger.getLogger(MoflonGenModelBuilder.class);
+public class MoflonGenModelBuilder extends GenModelBuilder {
+	protected static final Logger logger = Logger.getLogger(MoflonGenModelBuilder.class);
 
-   private static Optional<URIPreferenceExtension> uriPreferenceExtension;
+	private static Optional<URIPreferenceExtension> uriPreferenceExtension;
 
-   // The model file used for code generation
-   protected IFile ecoreFile;
+	// The model file used for code generation
+	protected IFile ecoreFile;
 
-   protected final URI ecoreURI;
+	protected final URI ecoreURI;
 
-   private final List<Resource> resources;
+	private final List<Resource> resources;
 
-   // The directory containing model file
-   private String modelDirectory;
+	// The directory containing model file
+	private String modelDirectory;
 
-   protected MoflonPropertiesContainer moflonProperties;
+	protected MoflonPropertiesContainer moflonProperties;
 
-   private String basePackage;
+	private String basePackage;
 
-   public MoflonGenModelBuilder(final ResourceSet resourceSet, final List<Resource> resources, final IFile ecoreFile, final String basePackage,
-         final String modelDirectory, final MoflonPropertiesContainer moflonProperties)
-   {
-      super(resourceSet);
-      this.resources = resources;
-      this.ecoreFile = ecoreFile;
-      this.basePackage = basePackage;
-      this.modelDirectory = modelDirectory;
-      this.moflonProperties = moflonProperties;
+	public MoflonGenModelBuilder(final ResourceSet resourceSet, final List<Resource> resources, final IFile ecoreFile,
+			final String basePackage, final String modelDirectory, final MoflonPropertiesContainer moflonProperties) {
+		super(resourceSet);
+		this.resources = resources;
+		this.ecoreFile = ecoreFile;
+		this.basePackage = basePackage;
+		this.modelDirectory = modelDirectory;
+		this.moflonProperties = moflonProperties;
 
-      final IProject project = ecoreFile.getProject();
+		final IProject project = ecoreFile.getProject();
 
-      final URI projectURI = determineProjectUriBasedOnPreferences(project);
-      this.ecoreURI = MoflonConventions.getDefaultProjectRelativeEcoreFileURI(project).resolve(projectURI);
-   }
+		final URI projectURI = determineProjectUriBasedOnPreferences(project);
+		this.ecoreURI = MoflonConventions.getDefaultProjectRelativeEcoreFileURI(project).resolve(projectURI);
+	}
 
-   @Override
-   public boolean isNewGenModelRequired(final URI genModelURI)
-   {
-      return super.isNewGenModelRequired(genModelURI) || moflonProperties.getReplaceGenModel().isBool();
-   }
+	@Override
+	public boolean isNewGenModelRequired(final URI genModelURI) {
+		return super.isNewGenModelRequired(genModelURI) || moflonProperties.getReplaceGenModel().isBool();
+	}
 
-   @Override
-   public void loadDefaultGenModelContent(final GenModel genModel)
-   {
-      super.loadDefaultGenModelContent(genModel);
-      genModel.getForeignModel().add(ecoreFile.getName());
-      genModel.setModelDirectory(modelDirectory); // org.gervarro.democles.emoflon/src
-      for (GenPackage genPackage : genModel.getGenPackages())
-      {
-         genPackage.setBasePackage(basePackage);
-      }
+	@Override
+	public void loadDefaultGenModelContent(final GenModel genModel) {
+		super.loadDefaultGenModelContent(genModel);
+		genModel.getForeignModel().add(ecoreFile.getName());
+		genModel.setModelDirectory(modelDirectory); // org.gervarro.democles.emoflon/src
+		for (GenPackage genPackage : genModel.getGenPackages()) {
+			genPackage.setBasePackage(basePackage);
+		}
 
-      // Enable generics
-      genModel.setComplianceLevel(GenJDKLevel.JDK80_LITERAL);
+		// Enable generics
+		genModel.setComplianceLevel(GenJDKLevel.JDK80_LITERAL);
 
-      // Enable operation reflection
-      genModel.setOperationReflection(true);
+		// Enable operation reflection
+		genModel.setOperationReflection(true);
 
-      // Set plugin id so conversion to plugin is available in editor
-      genModel.setModelPluginID(ecoreFile.getProject().getName());
-   }
+		// Set plugin id so conversion to plugin is available in editor
+		genModel.setModelPluginID(ecoreFile.getProject().getName());
+	}
 
-   @Override
-   public List<Dependency> getGenModelResourceDependencies()
-   {
-      BasicEList<Dependency> result = new BasicEList<Dependency>();
+	@Override
+	public List<Dependency> getGenModelResourceDependencies() {
+		BasicEList<Dependency> result = new BasicEList<Dependency>();
 
-      // User-defined GenPackages
-      for (final AdditionalUsedGenPackages usedDefinedGenPackage : moflonProperties.getAdditionalUsedGenPackages())
-      {
-         result.add(new SimpleDependency(URI.createURI(usedDefinedGenPackage.getValue())));
-      }
+		// User-defined GenPackages
+		for (final AdditionalUsedGenPackages usedDefinedGenPackage : moflonProperties.getAdditionalUsedGenPackages()) {
+			result.add(new SimpleDependency(URI.createURI(usedDefinedGenPackage.getValue())));
+		}
 
-      for (final Resource resource : resources)
-      {
-         final URI uri = resource.getURI();
-         if (!isAdditionalDependency(uri) && !uri.equals(ecoreURI))
-         {
-            final int kind = DependencyTypes.getDependencyType(uri);
-            if (kind == DependencyTypes.DEPLOYED_PLUGIN)
-            {
-               result.add(new SimpleDependency(lookupExtensionRegistry(uri)));
-            } else if (kind == DependencyTypes.WORKSPACE_PLUGIN_PROJECT || kind == DependencyTypes.WORKSPACE_PROJECT)
-            {
-               result.add(new SimpleDependency(calculateGenModelURI(uri)));
-            }
-         }
-      }
-      return result;
-   }
+		for (final Resource resource : resources) {
+			final URI uri = resource.getURI();
+			if (!isAdditionalDependency(uri) && !uri.equals(ecoreURI)) {
+				final int kind = DependencyTypes.getDependencyType(uri);
+				if (kind == DependencyTypes.DEPLOYED_PLUGIN) {
+					result.add(new SimpleDependency(lookupExtensionRegistry(uri)));
+				} else if (kind == DependencyTypes.WORKSPACE_PLUGIN_PROJECT
+						|| kind == DependencyTypes.WORKSPACE_PROJECT) {
+					result.add(new SimpleDependency(calculateGenModelURI(uri)));
+				}
+			}
+		}
+		return result;
+	}
 
-   /**
-    * Returns a platform:/ {@link URI} for the given project based on the corresponding preferences in {@link EMoflonPreferencesStorage}
-    * @param project the project
-    * @return the corresponding {@link URI}
-    */
-   public static URI determineProjectUriBasedOnPreferences(final IProject project)
-   {
-      final PlatformUriType preferredGenModelPlatformUriType = uriPreferenceExtension.orElse(PlatformUriType.PLUGIN);
-      final URI projectURI = determineProjectUriBasedOnPlatformUriType(project, preferredGenModelPlatformUriType);
-      return projectURI;
-   }
+	/**
+	 * Returns a platform:/ {@link URI} for the given project based on the
+	 * corresponding preferences in {@link EMoflonPreferencesStorage}
+	 * 
+	 * @param project
+	 *            the project
+	 * @return the corresponding {@link URI}
+	 */
+	public static URI determineProjectUriBasedOnPreferences(final IProject project) {
+		final PlatformUriType preferredGenModelPlatformUriType = uriPreferenceExtension.orElse(PlatformUriType.PLUGIN);
+		final URI projectURI = determineProjectUriBasedOnPlatformUriType(project, preferredGenModelPlatformUriType);
+		return projectURI;
+	}
 
-   /**
-    * Returns a platform:/ {@link URI} for the given project based on the given {@link PlatformUriType}
-    * @param project the project
-    * @param platformUriType the {@link PlatformUriType}
-    * @return the corresponding {@link URI}
-    */
-   private static URI determineProjectUriBasedOnPlatformUriType(final IProject project, final PlatformUriType platformUriType)
-   {
-      final URI projectURI;
-      switch (platformUriType)
-      {
-      case RESOURCE:
-         projectURI = eMoflonEMFUtil.lookupProjectURIAsPlatformResource(project);
-         break;
-      case PLUGIN:
-      default:
-         projectURI = eMoflonEMFUtil.lookupProjectURI(project);
-         break;
-      }
-      return projectURI;
-   }
+	/**
+	 * Returns a platform:/ {@link URI} for the given project based on the given
+	 * {@link PlatformUriType}
+	 * 
+	 * @param project
+	 *            the project
+	 * @param platformUriType
+	 *            the {@link PlatformUriType}
+	 * @return the corresponding {@link URI}
+	 */
+	private static URI determineProjectUriBasedOnPlatformUriType(final IProject project,
+			final PlatformUriType platformUriType) {
+		final URI projectURI;
+		switch (platformUriType) {
+		case RESOURCE:
+			projectURI = eMoflonEMFUtil.lookupProjectURIAsPlatformResource(project);
+			break;
+		case PLUGIN:
+		default:
+			projectURI = eMoflonEMFUtil.lookupProjectURI(project);
+			break;
+		}
+		return projectURI;
+	}
 
-   public static final URI calculateGenModelURI(final URI ecoreURI)
-   {
-      return ecoreURI.trimFileExtension().appendFileExtension(GENMODEL_FILE_EXTENSION);
-   }
+	public static final URI calculateGenModelURI(final URI ecoreURI) {
+		return ecoreURI.trimFileExtension().appendFileExtension(GENMODEL_FILE_EXTENSION);
+	}
 
-   private final boolean isAdditionalDependency(final URI uri)
-   {
-      for (AdditionalDependencies dependency : moflonProperties.getAdditionalDependencies())
-      {
-         if (dependency.getValue().equals(uri.toString()))
-         {
-            return true;
-         }
-      }
-      return false;
-   }
+	private final boolean isAdditionalDependency(final URI uri) {
+		for (AdditionalDependencies dependency : moflonProperties.getAdditionalDependencies()) {
+			if (dependency.getValue().equals(uri.toString())) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-   private final URI lookupExtensionRegistry(final URI uri)
-   {
-      assert uri.isPlatformPlugin() && uri.segmentCount() >= 2;
-      final String uriString = uri.toString();
-      final String pluginID = uri.segment(1);
-      final IExtensionPoint extensionPoint = Platform.getExtensionRegistry().getExtensionPoint("org.eclipse.emf.ecore.generated_package");
-      for (IExtension extension : extensionPoint.getExtensions())
-      {
-         if (pluginID.equals(extension.getContributor().getName()))
-         {
-            for (IConfigurationElement config : extension.getConfigurationElements())
-            {
-               if (uriString.equals(config.getAttribute("uri")))
-               {
-                  final String genModelAttribute = config.getAttribute("genModel");
-                  if (genModelAttribute != null)
-                  {
-                     final URI pluginURI = URI.createPlatformPluginURI(pluginID + "/", true);
-                     return URI.createURI(genModelAttribute).resolve(pluginURI);
-                  }
-               }
-            }
-            extension.getConfigurationElements()[0].getAttribute("genModel");
-         }
-      }
-      return calculateGenModelURI(uri);
-   }
+	private final URI lookupExtensionRegistry(final URI uri) {
+		assert uri.isPlatformPlugin() && uri.segmentCount() >= 2;
+		final String uriString = uri.toString();
+		final String pluginID = uri.segment(1);
+		final IExtensionPoint extensionPoint = Platform.getExtensionRegistry()
+				.getExtensionPoint("org.eclipse.emf.ecore.generated_package");
+		for (IExtension extension : extensionPoint.getExtensions()) {
+			if (pluginID.equals(extension.getContributor().getName())) {
+				for (IConfigurationElement config : extension.getConfigurationElements()) {
+					if (uriString.equals(config.getAttribute("uri"))) {
+						final String genModelAttribute = config.getAttribute("genModel");
+						if (genModelAttribute != null) {
+							final URI pluginURI = URI.createPlatformPluginURI(pluginID + "/", true);
+							return URI.createURI(genModelAttribute).resolve(pluginURI);
+						}
+					}
+				}
+				extension.getConfigurationElements()[0].getAttribute("genModel");
+			}
+		}
+		return calculateGenModelURI(uri);
+	}
 }
