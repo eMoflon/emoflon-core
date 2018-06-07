b39e23b4c8c7bf7f53cb52be0af7f9af5a0a51ff
Patrick Robrecht <mail@patrick-robrecht.de>
2018-06-07 15:25:13 +0200

    Move MoflonUtil.getFQN method to new EcoreUtils class, deprecated fallback

--- org.moflon.core.utilities/src/org/moflon/core/utilities/EcoreUtils.java
+++ org.moflon.core.utilities/src/org/moflon/core/utilities/EcoreUtils.java
@@ -3,6 +3,7 @@ package org.moflon.core.utilities;
 import java.util.Objects;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.ENamedElement;
 
 /**
  * Utility methods working with Ecore.
@@ -28,10 +29,8 @@ public class EcoreUtils {
 
 	/**
 	 * Checks whether the given EClasses are equal based on their fully qualified
-	 * names (determined with
-	 * {@link MoflonUtil#getFQN(org.eclipse.emf.ecore.ENamedElement)}. This is
-	 * sometimes necessary if a weaker equality is desirable (classes are loaded
-	 * from different resources).
+	 * names. This is sometimes necessary if a weaker equality is desirable (classes
+	 * are loaded from different resources).
 	 * 
 	 * @param class1
 	 *            an EClass
@@ -40,9 +39,27 @@ public class EcoreUtils {
 	 * @return true if and only if both classes have the same fully qualified name.
 	 */
 	public static boolean equalsFQN(final EClass class1, final EClass class2) {
-		if (class1 != null && class2 != null)
-			return MoflonUtil.getFQN(class1).equals(MoflonUtil.getFQN(class2));
-		else
+		if (class1 == null || class2 == null) {
 			return false;
+		}
+		return getFQN(class1).equals(getFQN(class2));
+	}
+
+	/**
+	 * Determine fully qualified name of given element by iterating through the
+	 * package hierarchy.
+	 *
+	 * @param ENamedElement
+	 *            the named element
+	 * @return the fully qualified name of the element
+	 */
+	public static String getFQN(final ENamedElement element) {
+		String fqn = element.getName();
+		ENamedElement e = element;
+		while (e.eContainer() != null) {
+			e = (ENamedElement) e.eContainer();
+			fqn = e.getName() + "." + fqn;
+		}
+		return fqn;
 	}
 }
