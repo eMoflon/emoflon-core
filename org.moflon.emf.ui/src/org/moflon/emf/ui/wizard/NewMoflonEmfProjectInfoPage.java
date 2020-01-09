2347f923281c572e006e2154021003f2e2f82e1a
SebastianE <sebastian.ehmes@gmail.com>
2020-01-09 15:32:44 +0100

    fixed a very old typo

--- org.moflon.emf.ui/src/org/moflon/emf/ui/wizard/NewMoflonEmfProjectInfoPage.java
+++ org.moflon.emf.ui/src/org/moflon/emf/ui/wizard/NewMoflonEmfProjectInfoPage.java
@@ -5,6 +5,6 @@ import org.moflon.core.ui.AbstractMoflonProjectInfoPage;
 public class NewMoflonEmfProjectInfoPage extends AbstractMoflonProjectInfoPage {
 
 	public NewMoflonEmfProjectInfoPage() {
-		super("NewMofonEmfProjectInfo", "New eMoflon EMF Project", "This wizard creates a new eMoflon EMF project.");
+		super("NewMoflonEmfProjectInfo", "New eMoflon EMF Project", "This wizard creates a new eMoflon EMF project.");
 	}
 }
