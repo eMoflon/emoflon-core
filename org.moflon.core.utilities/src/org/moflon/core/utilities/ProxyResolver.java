486037b48c6ca6f79bd68c719c669ae59dfa9110
Lars Fritsche <arikae00@jesitec.de>
2022-02-16 11:09:08 +0100

    mixed MANIFEST entries so that ProxyResolver is accessible from smartemf without causing cyclic dependencies

--- org.moflon.core.utilities/src/org/moflon/core/utilities/ProxyResolver.java
+++ org.moflon.core.utilities/src/org/moflon/core/utilities/ProxyResolver.java
@@ -1,18 +1,24 @@
 package org.moflon.core.utilities;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EPackage.Registry;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 public class ProxyResolver {
 	
@@ -22,6 +28,10 @@ public class ProxyResolver {
 	private static Map<URI, EClassifier> uri2class = new HashMap<>();
 
 	private static Map<URI, EPackage> uri2packages = new HashMap<>();
+	
+	// one resource set, which loads all ecores to resolve all proxies
+	private static ResourceSet rs = new ResourceSetImpl();
+
 
 	public static void resolveReference(EReference ref) {
 		ref.setEType(resolve(ref.getEType()));
@@ -34,11 +44,12 @@ public class ProxyResolver {
 		if(!((InternalEObject) obj).eIsProxy())
 			return obj;
 		
-		URI uri = ((InternalEObject) obj).eProxyURI();
+		URI classURI = ((InternalEObject) obj).eProxyURI();
+		URI uri = ((InternalEObject) obj).eProxyURI().trimFragment();
 		
 		// has it already been registered?
-		if(uri2class.containsKey(uri))
-			return uri2class.get(uri);
+		if(uri2class.containsKey(classURI))
+			return uri2class.get(classURI);
 		
 		// resolve containing package from URI
 		EPackage pkg = resolvePackage(uri);
@@ -59,15 +70,15 @@ public class ProxyResolver {
 		registerEClassifier(uri, pkg);
 		
 		// has it already been registered?
-		if(uri2class.containsKey(uri))
-			return uri2class.get(uri);
+		if(uri2class.containsKey(classURI))
+			return uri2class.get(classURI);
 		
 		// nothing to be done. cannot resolve it. blame EMF
 		return obj;
 	}
 	
 	private static void registerEClassifier(URI uri, EPackage pkg) {
-		String packageUri = uri.toString().substring(0, uri.toString().indexOf("#")) + "#//";
+		String packageUri = uri.toString() + "#//";
 		for(EClassifier ec : pkg.getEClassifiers()) {
 			uri2class.put(URI.createURI(packageUri + ec.getName()), ec);
 		}
@@ -81,21 +92,89 @@ public class ProxyResolver {
 		epackage.getEClassifiers().stream().filter(c -> c instanceof EClass).forEach(c -> ((EClass) c).getEReferences().forEach(ProxyResolver::resolveReference));;
 	}
 	
-	private static EPackage resolvePackage(URI uri) {
+	public static EPackage resolvePackage(URI uri) {
+		URI otherURI = null;
+		
+		if(uri.toString().contains("/resource/")) {
+			otherURI = URI.createURI(uri.toString().replace("/resource/", "/plugin/"));
+		}
+		if(uri.toString().contains("/plugin/")) {
+			otherURI = URI.createURI(uri.toString().replace("/plugin/", "/resource/"));
+		}
+		
+		// first execute with original uri than with the other
+		EPackage epkg = resolvePackageInternal(uri);
+		if(epkg == null) {
+			epkg = resolvePackageInternal(otherURI);
+			if(epkg != null) {
+				rs.getURIConverter().getURIMap().put(uri, otherURI);
+			}
+		}
+		return epkg;
+	}
+
+	public static EPackage resolvePackageInternal(URI uri) {
 		if(uri2packages.containsKey(uri)) {
 			return uri2packages.get(uri);
 		}
 		
-		try {
-			ResourceSet rs = new ResourceSetImpl();
-			Resource r = rs.createResource(uri);
-			r.load(null);
-			if(r.isLoaded()) {
-				return (EPackage) r.getContents().get(0);
+		EPackage epkg = Registry.INSTANCE.getEPackage(uri.toString());
+		if(epkg == null) {
+			try {
+				Resource r = rs.createResource(uri);
+				r.load(null);
+				if(r.isLoaded()) {
+					epkg = (EPackage) r.getContents().get(0);
+					uri2packages.put(uri, epkg);
+					EcoreUtil.resolveAll(rs);
+				}
+			}
+			catch(Exception e) {
 			}
 		}
-		catch(Exception e) {
+//		if(epkg != null)
+//			findAndFixProxies(epkg);
+
+		return epkg;
+	}
+
+	private static void findAndFixProxies(EPackage epkg) {
+		for(EPackage subPkg : epkg.getESubpackages()) {
+			findAndFixProxies(subPkg);
 		}
-		return null;
+		
+		// check if type is a proxy or any subtype, references or attribute types
+		for(EClassifier classifier : epkg.getEClassifiers()) {
+			if(classifier instanceof EClass eClass) {
+				Collection<EClass> newSuperTypes = new LinkedList<>();
+				for(EClass subClass : eClass.getEAllSuperTypes()) {
+					EClassifier resolve = ProxyResolver.resolve(subClass);
+					if(resolve == null) 
+						throw new RuntimeException("Could not resolve " + resolve);
+					newSuperTypes.add((EClass) resolve);
+				}
+//				eClass.getEAllSuperTypes().clear();
+//				eClass.getEAllSuperTypes().addAll(newSuperTypes);
+				
+				for(EReference ref : eClass.getEAllReferences()) {
+					if(ref.getEType().eIsProxy()) {
+						EClassifier resolve = ProxyResolver.resolve(ref.getEType());
+						if(resolve == null) 
+							throw new RuntimeException("Could not resolve " + resolve);
+						ref.setEType(resolve);
+					}
+				}
+				
+				for(EAttribute attr : eClass.getEAllAttributes()) {
+					if(attr.getEType().eIsProxy()) {
+						EClassifier resolve = ProxyResolver.resolve(attr.getEType());
+						if(resolve == null) 
+							throw new RuntimeException("Could not resolve " + resolve);
+						attr.setEType(resolve);
+					}
+				}
+			}
+		}
 	}
 }
+
