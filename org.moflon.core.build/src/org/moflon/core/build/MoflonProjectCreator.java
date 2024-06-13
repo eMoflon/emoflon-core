c4d81d61bf2990c63794eafefda709463cdf7c9b
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2024-06-13 15:23:38 +0200

    Updates Java to 21

--- org.moflon.core.build/src/org/moflon/core/build/MoflonProjectCreator.java
+++ org.moflon.core.build/src/org/moflon/core/build/MoflonProjectCreator.java
@@ -182,7 +182,7 @@ public abstract class MoflonProjectCreator extends WorkspaceTask implements Proj
 			changed |= ManifestFileUpdater.updateAttribute(manifest, PluginManifestConstants.BUNDLE_ACTIVATION_POLICY,
 					"lazy", AttributeUpdatePolicy.KEEP);
 			changed |= ManifestFileUpdater.updateAttribute(manifest,
-					PluginManifestConstants.BUNDLE_EXECUTION_ENVIRONMENT, "JavaSE-17", AttributeUpdatePolicy.FORCE);
+					PluginManifestConstants.BUNDLE_EXECUTION_ENVIRONMENT, "JavaSE-21", AttributeUpdatePolicy.FORCE);
 			changed |= ManifestFileUpdater.updateAttribute(manifest, PluginManifestConstants.AUTOMATIC_MODULE_NAME,
 					pluginId, AttributeUpdatePolicy.KEEP);
 			return changed;
